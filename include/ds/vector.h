015b3b988733a0d650a9fd18a79256f7dd077767
thisryan <55314284+thisryan@users.noreply.github.com>
2024-06-18 20:41:07 +0200

    Add first implementation of Vector (#1)
    * Add simple gitignore
    
    * Add Makefile
    
    * add docs folder
    
    * add documentation config and add build command to Makefile
    
    * Setup basic definitions
    
    * Setup important macros
    
    * Define Vector type
    
    * Add Function headers
    
    * Implement size function
    
    * Implement delete function to cleanup data
    
    * Implement push_back functions
    
    * Implement insert function
    
    * Implement pop_back function
    
    * Implement remove function
    
    * Undefine Macros
    
    * Add file documentation
    
    * Add doxygen ignores
    
    * Add size and delete defenitions
    
    * Only implement functions if implement header is added
    
    * Add doxygen documentation

---
+++ include/ds/vector.h
@@ -0,0 +1,230 @@
+/**
+ * @file
+
+ * @author  Adrian O
+ * @brief
+ * @version 1
+ * @date 2024-06-18
+ *
+ * @copyright Copyright (c) 2024
+ */
+
+ // This block only exists for better documentation
+#ifdef _DOXYGEN_
+
+/**
+ * @brief The exported Vector type
+ *
+ */
+typedef struct VECTOR_NAME {
+    /**
+     * @brief The items
+     *
+     */
+    VECTOR_T* data;
+
+    /**
+     * @brief The index behind the last written element
+     *
+     */
+    size_t index;
+
+    /**
+     * @brief The size of the data buffer
+     *
+     */
+    size_t size;
+} VECTOR_NAME;
+
+/**
+ * @brief Inserts element at the end of the vector
+ *
+ * @param v Pointer to the vector
+ * @param data element to be added
+ */
+void VECTOR_PREFIX_push_back(VECTOR_NAME* v, VECTOR_T data);
+
+/**
+ * @brief Insert element at specific index
+ *
+ * @param v Pointer to the vector
+ * @param data element to be added
+ * @param index the index of the insertion
+ */
+void VECTOR_PREFIX_insert(VECTOR_NAME* v, VECTOR_T data, size_t index);
+
+/**
+ * @brief Remove element from the end of the vector
+ *
+ * @param v Pointer to the vector
+ * @return VECTOR_T The removed element
+ */
+VECTOR_T VECTOR_PREFIX_pop_back(VECTOR_NAME* v);
+
+/**
+ * @brief Remove element from specified index
+ *
+ * @param v Pointer to the vector
+ * @param index the index of the deletion
+ * @return VECTOR_T the removed element
+ */
+VECTOR_T VECTOR_PREFIX_remove(VECTOR_NAME* v, size_t index);
+
+
+/**
+ * @brief This is the type which is stored inside the vector. This must be defined
+ *
+ */
+#define VECTOR_T
+
+ /**
+  * @brief The name of the created Vector Type. If not defined the name will be vector_VECTOR_T
+  *
+  */
+#define VECTOR_NAME
+
+  /**
+   * @brief The Prefix every function which operates on the vector has. If not defined the Prefix will be VECTOR_NAME
+   *
+   */
+#define VECTOR_PREFIX
+
+   /**
+    * @brief If defined there are two sets of functions, one with errors post fixed by _e and one without errors
+    *
+    */
+#define ERROR
+
+    /**
+     * @brief If defined the only defined functions are the error functions
+     *
+     */
+#define SAFE
+
+     /**
+      * @brief If set the functions are implemented
+      *
+      */
+#define IMPLEMENT
+#endif
+
+#ifndef VECTOR_HEADER_H
+// \cond
+#define VECTOR_HEADER_H
+// \endcond
+
+#include <stdlib.h>
+
+// \cond Ignore for Doxygen
+#define VECTOR_IMPL(word) VECTOR_COMB1(VECTOR_PREFIX,word)
+#define VECTOR_COMB1(pre, word) VECTOR_COMB2(pre, word)
+#define VECTOR_COMB2(pre, word) pre##word
+// \endcond
+
+#endif
+
+// \cond
+#ifndef VECTOR_T
+#error "VECTOR_T must be defined"
+#endif
+
+#ifndef VECTOR_NAME
+#define VECTOR_NAME VECTOR_COMB1(VECTOR_COMB1(vector,_), VECTOR_T)
+#endif
+
+#ifndef VECTOR_PREFIX
+#define VECTOR_PREFIX VECTOR_COMB1(VECTOR_NAME, _)
+#endif
+// \endcond
+
+// \cond
+typedef struct VECTOR_NAME VECTOR_NAME;
+struct VECTOR_NAME {
+    VECTOR_T* data;
+    size_t index;
+    size_t amount;
+};
+
+size_t VECTOR_IMPL(size)(VECTOR_NAME* v);
+void VECTOR_IMPL(delete)(VECTOR_NAME* v);
+
+
+void VECTOR_IMPL(push_back)(VECTOR_NAME* v, VECTOR_T data);
+void VECTOR_IMPL(insert)(VECTOR_NAME* v, VECTOR_T data, size_t index);
+VECTOR_T VECTOR_IMPL(pop_back)(VECTOR_NAME* v);
+VECTOR_T VECTOR_IMPL(remove)(VECTOR_NAME* v, size_t index);
+
+#ifdef IMPLEMENT
+
+size_t VECTOR_IMPL(size)(VECTOR_NAME* v) {
+    return v->index;
+}
+
+void VECTOR_IMPL(delete)(VECTOR_NAME* v) {
+    free(v->data);
+    v->amount = 0;
+    v->index = 0;
+}
+
+void VECTOR_IMPL(push_back)(VECTOR_NAME* v, VECTOR_T data) {
+    if (v->index >= v->amount) {
+        v->amount = v->amount ? v->amount * 2 : 1;
+        v->data = realloc(v->data, v->amount * sizeof(VECTOR_T));
+    }
+
+    v->data[v->index++] = data;
+}
+
+void VECTOR_IMPL(insert)(VECTOR_NAME* v, VECTOR_T data, size_t index) {
+    if (v->index >= v->amount) {
+        v->amount = v->amount ? v->amount * 2 : 1;
+        v->data = realloc(v->data, v->amount * sizeof(VECTOR_T));
+    }
+
+    if (index > v->index || index < 0) {
+        return;
+    }
+
+    for (size_t i = index;i < v->index;i++) {
+        v->data[i + 1] = v->data[i];
+    }
+
+    v->data[index] = data;
+    v->index++;
+}
+
+VECTOR_T VECTOR_IMPL(pop_back)(VECTOR_NAME* v) {
+    if (v->index == 0) {
+        return (VECTOR_T) { 0 };
+    }
+
+    return v->data[--v->index];
+}
+
+VECTOR_T VECTOR_IMPL(remove)(VECTOR_NAME* v, size_t index) {
+    if (index < 0 || index >= v->index) {
+        return (VECTOR_T) { 0 };
+    }
+
+    VECTOR_T store = v->data[index];
+    for (size_t i = index;i < v->index - 1;i++) {
+        v->data[i] = v->data[i + 1];
+    }
+    v->index--;
+
+    return store;
+}
+
+#endif
+
+#undef VECTOR_T
+#undef VECTOR_PREFIX
+#undef VECTOR_NAME
+#undef VECTOR_push_back
+#undef VECTOR_insert
+#undef VECTOR_pop_back
+#undef VECTOR_remove
+#undef IMPLEMENT
+#undef SAFE
+
+// \endcond
